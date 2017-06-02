699a65147e6f6969808f25b903acee7e4718bafd
吴 <514965273@qq.com>
2017-06-02 17:54:01 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -55,12 +55,9 @@ local _M = {
     config_waf_redirect_url = "http://xsec.io",
     config_expire_time = 600,
     config_output_html=[[
-    <html lang="zh-cn">
+        <html lang="zh-cn">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="data-spm" content="a3c0e"/>
-
         <title>405</title>
         
         <style>
@@ -74,36 +71,25 @@ local _M = {
             .main { padding: 50px 0; background: #f4f5f7; }
             .main img { position: relative; left: 120px; }
             
-            .footer { margin-top: 30px; text-align: right; }
-            .footer a { padding: 8px 30px; border-radius: 10px; border: 1px solid #4babec; }
-            .footer a:hover { opacity: .8; }
-            
-            .alert-shadow { display: none; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: #999; opacity: .5; }
-            .alert { display: none; position: absolute; top: 200px; left: 50%; width: 600px; margin-left: -300px; padding-bottom: 25px; border: 1px solid #ddd; box-shadow: 0 2px 2px 1px rgba(0, 0, 0, .1); background: #fff; font-size: 14px; color: #696969; }
-            .alert h2 {  margin: 0 2px; padding: 10px 15px 5px 15px; font-size: 14px; font-weight: normal; border-bottom: 1px solid #ddd; }
-            .alert a { display: block; position: absolute; right: 10px; top: 8px; width: 30px; height: 20px; text-align: center; }
-            .alert p {  padding: 20px 15px; }
         </style>
 
     </head>
-        
-    <body data-spm="7663354">
-        <div data-spm="1998410538">
+    <body>
+        <div>
             <div class="header">
                 <div class="container">
                     <div class="message">很抱歉，由于您访问的URL有可能对网站造成安全威胁，您的访问被阻断。</div>
-					
+
                 </div>
             </div>
             <div class="main">
                 <div class="container">
                     <img src="https://errors.aliyun.com/images/TB15QGaHpXXXXXOaXXXXia39XXX-660-117.png" />
                 </div>
-            </div>
-            
+            </div> 
         </div>
     </body>
-</html>
+    </html>
     ]],
 }
 
